5c8b365396c6494f1fe6b7cb9d081e4b9f1da478
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2020-02-14 15:53:57 +0800

    Ideologies

---
+++ ideologies.js
@@ -0,0 +1,205 @@
+ideologies = [
+	{
+        "name": "Fascism",
+		"desc": "Fascism is an ideology that favours a centrlized government. Fascists think a nation's power should be put a high importance.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 80,
+            "b": 85,
+            "c": 90,
+            "d": 90,
+			"e": 15,
+			"f": 90,
+			"g": 70
+        }
+    },
+    {
+        "name": "Nazism",
+		"desc": "Nazism is a form of Fascism that is a bit more extreme, and values the race of people more. It favours a centrlized government.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 80,
+            "b": 90,
+            "c": 95,
+            "d": 5,
+			"e": 5,
+			"f": 90,
+			"g": 100
+        }
+    },
+	{
+        "name": "Monarchism",
+		"desc": "Monarchism is a ideology that advocates a large government with a dictatorship, which the leadership is passed on by genetics.",
+        "i18n": {
+        },
+        "stats": {
+            "a": 30,
+            "b": 50,
+            "c": 80,
+            "d": 30,
+			"e": 80,
+			"f": 50,
+			"g": 70
+        }
+    },
+	{
+        "name": "Feudalism",
+		"desc": "Feudalism is an ideology that is based on Monarchism. It is basically a monarchy with high importance of capitalism. Higher classes rule over lower classes' property in exchange for land.",
+        "i18n": {
+        },
+        "stats": {
+            "a": 30,
+            "b": 20,
+            "c": 70,
+            "d": 30,
+			"e": 80,
+			"f": 50,
+			"g": 70
+        }
+    },
+	{
+        "name": "Constitutional Monarchism",
+		"desc": "Constitutional Monarchism is a form of Monarchism where the leader has a lot of power, but needs to follow a constitution to rule.",
+        "i18n": {
+        },
+        "stats": {
+            "a": 20,
+            "b": 30,
+            "c": 60,
+            "d": 30,
+			"e": 80,
+			"f": 50,
+			"g": 70
+        }
+    },
+	{
+        "name": "Conservatism",
+		"desc": "Conservatism is an ideology that is mainly about preserving the western political model. They want a small government, a large military spending, and low taxes.",
+        "i18n": {
+        },
+        "stats": {
+            "a": 10,
+            "b": 20,
+            "c": 40,
+            "d": 70,
+			"e": 70,
+			"f": 50,
+			"g": 10
+        }
+    },
+	{
+        "name": "Neo-Conservatism",
+		"desc": "Neo-Conservatism is a new form of Conservatism. Neo-Conservatives think that the goal of western nations is to bring their model to the world. They are more internaitonally focused.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 10,
+            "b": 20,
+            "c": 40,
+            "d": 90,
+            "e": 60,
+            "f": 50,
+            "g": 10
+        }
+    },
+	{
+        "name": "Classic Liberalism",
+		"desc": "Classic Liberalism is an ideology that advocates individualism, and also favours a larger government compared to that of Conservatives. They value personal property.",
+        "i18n": {
+        },
+        "stats": {
+            "a": 20,
+            "b": 10,
+            "c": 40,
+            "d": 70,
+			"e": 50,
+			"f": 50,
+			"g": 10
+        }
+    },
+	{
+        "name": "Libetarianism",
+		"desc": "Libetarianism is an ideology that wants a very small government. They think the purpose of the government is only for protecting and enforcing laws. This type of government is called the Night-Watchman State.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 50,
+            "b": 30,
+            "c": 20,
+            "d": 50,
+			"e": 50,
+			"f": 30,
+			"g": 20
+        }
+    },
+	{
+        "name": "Individualist Anarchism",
+		"desc": "Individualist Anarchism is a form of anarchism which strongly advocates individualism, alongside with a anarchist governement.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 30,
+            "b": 20,
+            "c": 10,
+            "d": 50,
+			"e": 50,
+			"f": 60,
+			"g": 10
+        }
+    },
+	{
+        "name": "Technocracy",
+		"desc": "Technocracy is a ideology that promotes the idea of people who are experts of technology shuold rule the nation. ",
+        "i18n": {
+           
+        },
+        "stats": {
+            "a": 80,
+            "b": 20,
+            "c": 10,
+            "d": 40,
+			"e": 30,
+			"f": 80,
+			"g": 20
+        }
+    },
+	{
+        "name": "Agorism",
+		"desc": "Agorists think that people could live in harmony without a state in an anarchist society, called the non-agression principle. They favour a free market.",
+        "i18n": {
+            
+        },
+        "stats": {
+            "a": 70,
+            "b": 20,
+            "c": 10,
+            "d": 80,
+			"e": 50,
+			"f": 50,
+			"g": 10
+        }
+    }
+    {
+        "name": "Anarcho-Capitalism",
+        "desc": "Anarcho-Capitalism is an anarchist ideology that wants big cooperations to rule over society. It is an extreme form of anarchy and capitalism. ",
+        "i18n": {
+           
+        },
+        "stats": {
+            "a": 70,
+            "b": 0,
+            "c": 10,
+            "d": 80,
+            "e": 50,
+            "f": 50,
+            "g": 10
+        }
+
+];
