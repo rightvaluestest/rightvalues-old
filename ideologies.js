525ebec60db94370485142980771c863c3ee16b8
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2020-02-16 16:09:32 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,7 +1,7 @@
 ideologies = [
 	{
-        "name": "Marxism-Leninism",
-		"desc": "Marxism-Leninism is a form of Marxism that was forged in the 20th century in the Soviet Union. Marxist-Leninists heavily favor the use of a communist political party as the platform for both achieving revolution and establishing socialism. Many Marxist-Leninists are somewhat more nationalistic and patriotic than many other Marxists, and may favor industrial progress over environmental goals.",
+        "name": "Fascism",
+		"desc": "Fascism is an ideology that favours a centrlized government. Fascists think a nation's power should be put a high importance.",
         "i18n": {
             "de": {
                 "name": "Marxismus-Leninismus",
@@ -14,17 +14,17 @@ ideologies = [
         },
         "stats": {
             "a": 80,
-            "b": 80,
+            "b": 85,
             "c": 90,
-            "d": 50,
-			"e": 90,
-			"f": 70,
-			"g": 50
+            "d": 90,
+			"e": 15,
+			"f": 90,
+			"g": 70
         }
     },
     {
-        "name": "Orthodox Marxism",
-		"desc": "Orthodox Marxism is a form of Marxism that adopts views that conform to Marxist theory as it was originally written, particularily on the topic of dialectical materialism. Orthodox Marxists are highly revolutionary and internationalist and hold that nonindustrial societies are incapable of achieving socialism.",
+        "name": "Nazism",
+		"desc": "Nazism is a form of Fascism that is a bit more extreme, and values the race of people more. It favours a centrlized government.",
         "i18n": {
             "de": {
                 "name": "Orthodoxer Marxismus",
@@ -37,17 +37,17 @@ ideologies = [
         },
         "stats": {
             "a": 80,
-            "b": 80,
-            "c": 60,
-            "d": 80,
-			"e": 70,
-			"f": 50,
-			"g": 30
+            "b": 90,
+            "c": 95,
+            "d": 5,
+			"e": 5,
+			"f": 90,
+			"g": 100
         }
     },
 	{
-        "name": "Eco-Marxism",
-		"desc": "Eco-Marxism is a form of Orthodox Marxism more commonly found in the modern day that places a particular emphasis on environmental issues. Eco-Marxists generally favor central economic planning as a way of combating climate issues.",
+        "name": "Monarchism",
+		"desc": "Monarchism is a ideology that advocates a large government with a dictatorship, which the leadership is passed on by genetics.",
         "i18n": {
             "de": {
                 "name": "Öko-Marxismus",
@@ -59,18 +59,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 80,
-            "b": 80,
-            "c": 60,
-            "d": 80,
-			"e": 70,
-			"f": 20,
-			"g": 30
+            "a": 30,
+            "b": 50,
+            "c": 80,
+            "d": 30,
+			"e": 80,
+			"f": 50,
+			"g": 70
         }
     },
 	{
-        "name": "Centrist Marxism",
-		"desc": "Centrist Marxism is a form of Marxism that adopts Marxist views on society and the economy while also refraining from taking a definitive position on revolution and reformism. Many Centrist Marxists may also be more nationalistic than other Marxists.",
+        "name": "Feudalism",
+		"desc": "Feudalism is an ideology that is based on Monarchism. It is basically a monarchy with high importance of capitalism. Higher classes rule over lower classes' property in exchange for land.",
         "i18n": {
             "de": {
                 "name": "Zentristischer Marxismus",
@@ -82,18 +82,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 40,
-            "b": 80,
-            "c": 50,
-            "d": 50,
-			"e": 60,
-			"f": 40,
-			"g": 40
+            "a": 30,
+            "b": 20,
+            "c": 70,
+            "d": 30,
+			"e": 80,
+			"f": 50,
+			"g": 70
         }
     },
 	{
-        "name": "Council Communism",
-		"desc": "Council Communism is a form of Marxism that advocates for workers councils being the core foundation of a socialist society. Council Communists generally oppose the existence of strong influence from political parties within the socialist political system, favoring council democracy.",
+        "name": "Constitutional Monarchism",
+		"desc": "Constitutional Monarchism is a form of Monarchism where the leader has a lot of power, but needs to follow a constitution to rule.",
         "i18n": {
             "de": {
                 "name": "Konzilkommunismus",
@@ -105,18 +105,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 90,
-            "b": 70,
-            "c": 30,
-            "d": 80,
-			"e": 40,
+            "a": 20,
+            "b": 30,
+            "c": 60,
+            "d": 30,
+			"e": 80,
 			"f": 50,
-			"g": 30
+			"g": 70
         }
     },
 	{
-        "name": "Left Communism",
-		"desc": "Left Communism is a form of Marxism that leans towards so called ultra-left positions. These include unconditional support for revolution and internationalism, a rejection of unions and participation in liberal democracy, and a rejection of national liberation and self-determination.",
+        "name": "Conservatism",
+		"desc": "Conservatism is an ideology that is mainly about preserving the western political model. They want a small government, a large military spending, and low taxes. It is the one of the most popular Rightist ideology in the Western World.",
         "i18n": {
             "de": {
                 "name": "Linkskommunismus",
@@ -128,18 +128,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 100,
-            "b": 60,
-            "c": 50,
-            "d": 80,
-			"e": 80,
+            "a": 10,
+            "b": 20,
+            "c": 40,
+            "d": 70,
+			"e": 70,
 			"f": 50,
-			"g": 30
+			"g": 10
         }
     },
 	{
-        "name": "Anarcho-Communism",
-		"desc": "Anarcho-Communism is a form of anarchism that advocates for the abolition of capitalism and the state, as well as the collective ownership of the means of production. Anarcho-Communists support a decentralized economy and government, with Anarcho-Syndicalism being a common way of organizing such a society.",
+        "name": "Liberalism",
+		"desc": "Liberalism is an left-leaning ideology that advocates for policies like state ownership. They don't support Capitalism.",
         "i18n": {
             "de": {
                 "name": "Anarcho-Kommunismus",
@@ -151,18 +151,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 80,
-            "b": 50,
-            "c": 20,
-            "d": 80,
-			"e": 10,
+            "a": 70,
+            "b": 60,
+            "c": 50,
+            "d": 60,
+			"e": 70,
 			"f": 50,
-			"g": 30
+			"g": 20
         }
     },
 	{
-        "name": "Eco-Anarchism",
-		"desc": "Eco-Anarchism, or Green Anarchism, is a form of anarchism that places a particular emphasis on environmental issues. It is often linked to more distinct ideologies such as Anarcho-Syndicalism. Eco-Anarchists are generally revolutionary and support using a decentralized egalitarian economy to achieve environmental goals.",
+        "name": "Neo-Conservatism",
+		"desc": "Neo-Conservatism is a new form of Conservatism. Neo-Conservatives think that the goal of western nations is to bring their model to the world. They are more internationally focused.",
         "i18n": {
             "de": {
                 "name": "Öko-Anarchismus",
@@ -174,18 +174,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 80,
-            "b": 50,
-            "c": 20,
-            "d": 80,
-			"e": 10,
-			"f": 20,
-			"g": 30
+            "a": 10,
+            "b": 20,
+            "c": 40,
+            "d": 90,
+			"e": 60,
+			"f": 50,
+			"g": 10
         }
     },
 	{
-        "name": "Market Anarchism",
-		"desc": "Market Anarchism is a form of anarchism that mixes anarchist goals such as the abolition of hierarchy, the state and capitalism with heightened individualism and a moderate reformist approach. This should not be confused with Anarcho-Capitalism.",
+        "name": "Libertarianism",
+		"desc": "Libetarianism is an ideology that wants a very small government. They think the purpose of the government is only for protecting and enforcing laws. This type of government is called the Night-Watchman State",
         "i18n": {
             "de": {
                 "name": "Marktanarchismus",
@@ -197,18 +197,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 20,
-            "b": 40,
-            "c": 30,
-            "d": 70,
-			"e": 10,
-			"f": 20,
-			"g": 30
+            "a": 50,
+            "b": 30,
+            "c": 20,
+            "d": 50,
+			"e": 50,
+			"f": 30,
+			"g": 20
         }
     },
 	{
-        "name": "Utopian Socialism",
-		"desc": "Utopian Socialism is a form of pre-Marxist socialism that belives highly in an egalitarian, moralistic and idealistic foundation for a socialist society. Utopian Socialists generally reject violent revolution and often believe the ruling class can be convinced to adopt socialism.",
+        "name": "Individualist Anarchism",
+		"desc": "Individualist Anarchism is a form of anarchism which strongly advocates individualism, alongside with a anarchist governement.",
         "i18n": {
             "de": {
                 "name": "Utopischer Sozialismus",
@@ -222,16 +222,16 @@ ideologies = [
         "stats": {
             "a": 30,
             "b": 20,
-            "c": 40,
-            "d": 20,
+            "c": 10,
+            "d": 50,
 			"e": 50,
-			"f": 40,
-			"g": 50
+			"f": 60,
+			"g": 10
         }
     },
 	{
-        "name": "Democratic Socialism",
-		"desc": "Democratic Socialism is a form of socialism that seeks to utilize liberal democracy as a means to achieve a socialist economy and society. Democratic Socialist reject revolution and a centrally planned economy, instead supporting moderate social ownership in the form of publicly owned utilities and democratic workplace self-management.",
+        "name": "Agorism",
+		"desc": "Agorists think that people could live in harmony without a state in an anarchist society, called the non-agression principle. They favour a free market.",
         "i18n": {
             "de": {
                 "name": "Demokratischer Sozialismus",
@@ -243,18 +243,18 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 30,
-            "b": 30,
-            "c": 30,
-            "d": 60,
-			"e": 60,
-			"f": 30,
-			"g": 20
+            "a": 70,
+            "b": 20,
+            "c": 10,
+            "d": 80,
+			"e": 50,
+			"f": 50,
+			"g": 10
         }
     },
 	{
-        "name": "Social Democracy",
-		"desc": "Social Democracy is a centre-left ideology that advocates for mixing left-leaning values such as social welfare and corporate regulation with capitalism and liberal democracy in the form of a mixed economy. Many modern Social Democrats favor Keynesian economics.",
+        "name": "Anarcho-Capitalism",
+		"desc": "Anarcho-Capitalism is an anarchist ideology that wants big cooperations to rule over society. It is an extreme form of anarchy and capitalism.",
         "i18n": {
             "de": {
                 "name": "Sozialdemokratie",
@@ -266,13 +266,13 @@ ideologies = [
             }
         },
         "stats": {
-            "a": 20,
-            "b": 30,
-            "c": 50,
-            "d": 40,
-			"e": 80,
+            "a": 70,
+            "b": 0,
+            "c": 10,
+            "d": 80,
+			"e": 50,
 			"f": 50,
-			"g": 30
+			"g": 10
         }
     }
 ];
