08830dc7af9d5ac2672594898d34ecae2dbee5f1
rightvaluestest <61039650+rightvaluestest@users.noreply.github.com>
2020-02-17 11:04:29 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -143,9 +143,9 @@ const i18n = {
         "ko": `어떠한 범주에서든 100%의 점수를 얻을 수 없다는 것을 기억하십시오. 이 설문의 목적은 당신 자신의 관점에 직면하는 것에 있습니다. 의견이나 건설적인 비판은 <a href="https://forms.gle/6WZYMb5GXkkDLhxr5">이 양식</a>이나 <a href="https://github.com/LeftValues/leftvalues.github.io">GitHub Page</a>의 이슈를 통해 개진할 수 있습니다.`
     },
        "result-a-label": {
-        "en": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Leaning Right","Conservative","Preservationist"],
-        "de": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Leaning Right","Conservative","Preservationist"],
-        "ko": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Leaning Right","Conservative","Preservationist"]
+        "en": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Right","Conservative","Preservationist"],
+        "de": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Right","Conservative","Preservationist"],
+        "ko": ["Culturally Socialist","Culturally Liberal","Leaning Left","Neutral","Right","Conservative","Preservationist"]
     },
     "result-b-label": {
         "en": ["Extremely Pro-State Ownership","Pro-State Ownership","Public Ownership","Neutral","Laissez-faire","Markets","Extremely Pro-Markets"],
@@ -153,9 +153,9 @@ const i18n = {
         "ko": ["Extremely Pro-State Ownership","Pro-State Ownership","Public Ownership","Neutral","Laissez-faire","Markets","Extremely Pro-Markets"]
     },
     "result-c-label": {
-        "en": ["Totalitarian","Very Centralist","Centralist","Neutral","Democratic","Libertarian","Anarchist"],
-        "de": ["Totalitarian","Very Centralist","Centralist","Neutral","Democratic","Libertarian","Anarchist"],
-        "ko": ["Totalitarian","Very Centralist","Centralist","Neutral","Democratic","Libertarian","Anarchist"]
+        "en": ["Totalitarian","Authoritarian","Centralist","Neutral","Democratic","Libertarian","Anarchist"],
+        "de": ["Totalitarian","Authoritarian","Centralist","Neutral","Democratic","Libertarian","Anarchist"],
+        "ko": ["Totalitarian","Authoritarian","Centralist","Neutral","Democratic","Libertarian","Anarchist"]
     },
     "result-d-label": {
         "en": ["Extremely Internationalist","Internationalist","Pro-Alliances","Neutral","Nationalist","Patriot","Extremely Nationalist"],
